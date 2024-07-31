32c76658688b4c8b7337202912f1f78e899054cd
Eduardo Jirón <emjironal@gmail.com>
2024-07-31 15:53:25 -0600

    [update] add profile picture

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,11 +1,12 @@
 import "./Information.css";
 import LinkedInButton from "./LinkedInButton";
 import MailButton from "./MailButton";
+import profileImage from "../../assets/profile.jpeg";
 
 export default function Information() {
   return (
     <div className="container">
-      <img className="profile-image" />
+      <img className="profile-image" src={profileImage} />
       <h1 className="name">Eduardo Jirón</h1>
       <h4 className="position">Software Engineer</h4>
       <a
