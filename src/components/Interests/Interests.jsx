cb4f7ba99a02f27cbfa9bd62084fd696d5f31988
Eduardo Jirón <emjironal@gmail.com>
2024-07-31 19:00:50 -0600

    [update] create interests component

---
+++ src/components/Interests/Interests.jsx
@@ -0,0 +1,11 @@
+export default function Interests() {
+  return (
+    <div className="interests--container">
+      <h1 className="interests--title">About</h1>
+      <p className="interests--text">
+        Grill master. Music lover. Gamer. Data enthusiat. Camping geek. Coffee
+        fanatic. Anime enjoyer. Autodidacticism.
+      </p>
+    </div>
+  );
+}
