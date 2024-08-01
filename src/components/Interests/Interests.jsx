fd572c86935492565119cb3f8a371f89c361b100
Eduardo Jirón <emjironal@gmail.com>
2024-07-31 19:16:55 -0600

    [feature] add styling to interests component

--- src/components/Interests/Interests.jsx
+++ src/components/Interests/Interests.jsx
@@ -1,7 +1,9 @@
+import "./Interests.css"
+
 export default function Interests() {
   return (
     <div className="interests--container">
-      <h1 className="interests--title">About</h1>
+      <h1 className="interests--title">Interests</h1>
       <p className="interests--text">
         Grill master. Music lover. Gamer. Data enthusiat. Camping geek. Coffee
         fanatic. Anime enjoyer. Autodidacticism.
