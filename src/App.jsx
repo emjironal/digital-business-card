52e420f69d47e9ac1e229300434af864fb747528
Eduardo Jirón <emjironal@gmail.com>
2024-08-01 11:21:42 -0600

    [update] add footer component to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import profileImage from "./assets/profile.jpeg";
 import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
 import Information from "./components/Information/Information";
 import Interests from "./components/Interests/Interests";
 
@@ -12,6 +13,7 @@ function App() {
                 <Information />
                 <About />
                 <Interests />
+                <Footer />
             </div>
         </>
     );
